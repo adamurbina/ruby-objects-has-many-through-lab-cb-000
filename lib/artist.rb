3148dcdf3f12b05cfaba8dd06612f3c8c07682c4
Adam Urbina <adamurbina@gmail.com>
2018-06-14 10:36:43 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,22 @@
 class Artist
 
-    attr_accessor :name
+    attr_accessor :name, :songs, :genre
+
+    @@all = []
 
     def initialize(name)
         @name = name
+        @@all << self
+        @songs = []
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_song(name, genre)
+        song = Song.new(name, self, genre)
+        @songs << song
     end
 
 end
